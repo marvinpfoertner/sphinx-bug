f1941da38d3aed8c934c96a678ccca34f38d654e
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-01-27 10:01:03 +0100

    Add yields

--- src/test_module.py
+++ src/test_module.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterator
+
+
 def test_one_return_type_in_docstring():
     """Test function with one return value and return type documentation in the
     docstring.
@@ -47,3 +50,53 @@ def test_two_returns() -> tuple[int, float]:
         Description of ``return2``.
     """
     return 42, 42.0
+
+
+def test_one_yield_type_in_docstring():
+    """Test function yielding one value.
+
+    Yields
+    ------
+    yield1 : int
+        Description of ``yield1``.
+    """
+    yield 1
+
+
+def test_two_yields_type_in_docstring():
+    """Test function yielding a tuple of two values.
+
+    Yields
+    ------
+    yield1 : int
+        Description of ``yield1``.
+
+    yield2 : float
+        Description of ``yield2``.
+    """
+    yield 1, 2.0
+
+
+def test_one_yield() -> Iterator[int]:
+    """Test function yielding one value.
+
+    Yields
+    ------
+    yield1 :
+        Description of ``yield1``.
+    """
+    yield 1
+
+
+def test_two_yields() -> Iterator[tuple[int, float]]:
+    """Test function yielding a tuple of two values.
+
+    Yields
+    ------
+    yield1 :
+        Description of ``yield1``.
+
+    yield2 :
+        Description of ``yield2``.
+    """
+    yield 1, 2.0
